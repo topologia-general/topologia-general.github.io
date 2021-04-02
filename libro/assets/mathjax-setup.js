eb1b2654219f6b69cf8b77eee1b069cae62961da
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2021-04-02 19:59:33 +1000

    casi listo para todo el libro

---
+++ libro/assets/mathjax-setup.js
@@ -0,0 +1,53 @@
+/*MathJax.Hub.Config({
+  TeX: {
+    equationNumbers: {
+      autoNumber: "AMS"
+    },
+    extensions: ["AMSmath.js", "AMSsymbols.js"],
+    Macros: {
+      bh: "{\\mathbb{H}}",
+      bt: "{$\\blacktriangleleft$}",
+      btt: "{$\\blacktriangleright$}",
+      acc: "{\\curvearrowright}",
+      rp: ["{\\mathbb{RP}^{#1}}", 1],
+      cp: ["{\\mathbb{CP}^{#1}}", 1],
+      ida: "{\\boxed{\\Rightarrow}}",
+      vuelta: "{\\boxed{\\Leftarrow}}",
+      simi: "{\\!\sim}",
+      dem: "{\\underline{Demostraci\'on.}}",
+      findem: "{$\\;\\blacksquare$}",
+      bk: "{\\mathbb{K}}",
+      ff: "{\\mathbb{F}}",
+      br: "{\\mathbb{R}}",
+      bn: "{\\mathbb{N}}",
+      bz: "{\\mathbb{Z}}",
+      bc: "{\\mathbb{C}}",
+      bq: "{\\mathbb{Q}}",
+      ra: "{\\rightarrow}",
+      lra: "{\\longrightarrow}",
+      sss: "{\\Longleftrightarrow}",
+      stac: ["{\\stackrel{#1}{\\longrightarrow}}", 1],
+      Ra: "{\\Rightarrow}",
+      res: ["{{\\it\\textbf{#1}}}", 1],
+      barra: "{\\;|\\;}",
+      topp: "{\\rm Top}",
+      baca: "{\\backslash}",
+      til: ["{\\tilde{#1}}", 1],
+      mcal: ["{\\mathcal #1}", 1],
+      ol: ["{\\overline{#1}}", 1],
+      map: "{\\mbox{Map}}",
+      rel: "{\\: \\mbox{rel}}"
+    }
+  }
+});
+
+MathJax.Hub.Config({
+  tex2jax: {
+    inlineMath: [
+      ['$', '$'],
+      ['\\(', '\\)']
+    ],
+    processEscapes: true
+  }
+});
+*/
