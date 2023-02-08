22bea04111887dc6d1803f197325c2505f918436
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2023-02-08 19:45:26 +1000

    ok

--- libro/assets/scripts.js
+++ libro/assets/scripts.js
@@ -7,3 +7,14 @@ function w3_open() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
   }
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+    
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+}
