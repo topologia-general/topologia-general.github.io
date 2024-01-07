75404e2614fd64ec4cd613776b79a1d9cd6ac7b8
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2024-01-08 09:05:15 +1000

    ok

--- libro/assets/scripts.js
+++ libro/assets/scripts.js
@@ -9,6 +9,7 @@ function w3_open() {
   }
 
 // When the user scrolls down 20px from the top of the document, slide down the navbar
+/*
 window.onscroll = function() {scrollFunction()};
     
 function scrollFunction() {
@@ -18,3 +19,4 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "-100px";
   }
 }
+*/
